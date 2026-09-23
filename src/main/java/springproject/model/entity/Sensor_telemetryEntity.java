2cbe17818e14c6fc08c8dd4be975ef79f5ef40e4
ericj706 <ericj6215@gmail.com>
2026-09-23 18:12:57 +0900

    Entity취합

--- src/main/java/springproject/model/entity/Sensor_telemetryEntity.java
+++ src/main/java/springproject/model/entity/Sensor_telemetryEntity.java
@@ -59,5 +59,5 @@ public class Sensor_telemetryEntity {
 
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
-    private UsersEntity userEntity;
+    private UsersEntity usersEntity;
 }
