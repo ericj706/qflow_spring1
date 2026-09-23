c3ae6b10c5cde8f7b1851efd458159753846df5f
ericj706 <ericj6215@gmail.com>
2026-09-23 18:08:57 +0900

    Entity취합

--- src/main/java/springproject/model/entity/data_change_logEntity.java
+++ src/main/java/springproject/model/entity/data_change_logEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -65,7 +66,7 @@ public class data_change_logEntity extend BaseTime{
     @JoinColumn(name="user_id") 
     @ManyToOne 
     @Column(name = "user_id")
-    private Integer user_id;
+    private UsersEntity usersEntity;
 
     /*
      변경사유
